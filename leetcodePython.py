27ac9e3424073aecf3bb2b99f4d5351ad1a7e0dd
Ian Lumiere <ianmlumiere@gmail.com>
2018-08-23 17:33:00 -0700

    Update leetcodePython.py

--- leetcodePython.py
+++ leetcodePython.py
@@ -459,6 +459,54 @@ def sumPairs(num, data):
             
     return answers
 
+# International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
 
-    
+# For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+# [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+# Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+# Return the number of different transformations among all words we have.
+
+# Example:
+# Input: words = ["gin", "zen", "gig", "msg"]
+# Output: 2
+# Explanation: 
+# The transformation of each word is:
+# "gin" -> "--...-."
+# "zen" -> "--...-."
+# "gig" -> "--...--."
+# "msg" -> "--...--."
+
+# There are 2 different transformations, "--...-." and "--...--.".
+
+class Solution:
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morseCode = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+                     "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-",
+                     "...-",".--","-..-","-.--","--.."]
+
         
+        # create a hash for the words to store the count that they appear in
+        transformations = {}
+        
+        for i in words:
+            # translate i to morse code
+            translatedWord = ""
+            
+            for letter in i:
+                # get the ASCII, then subtract to correspond it to the index of the array
+                letterFixed = letter.upper()
+                translatedWord += morseCode[ord(letterFixed)-65]
+                
+            # Add the word to the keys in transformations
+            if translatedWord not in transformations:
+                transformations[translatedWord] = 1
+            else:
+                transformations[translatedWord] += 1
+        
+        return len(transformations)
