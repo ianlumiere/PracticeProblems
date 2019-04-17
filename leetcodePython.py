4d4bf90cab425111d2dace79405741c2602c378a
Ian Lumiere <ianmlumiere@gmail.com>
2019-04-16 20:37:42 -0700

    Update leetcodePython.py

--- leetcodePython.py
+++ leetcodePython.py
@@ -511,3 +511,56 @@ class Solution:
         
         # You can also return the number of times a transformation appears
         return len(transformations) # O(nm)
+
+    
+# Given a 32-bit signed integer, reverse digits of an integer.
+
+# Example 1:
+
+# Input: 123
+# Output: 321
+# Example 2:
+
+# Input: -123
+# Output: -321
+# Example 3:
+
+# Input: 120
+# Output: 21
+# Note:
+# Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x > (2**31 - 1):
+            return 0
+        elif x < -2**31:
+            return 0
+                
+        # check if negative
+        negative = False
+        if x < 0:
+            numString = str(x * -1)
+            negative = True
+        else:
+            numString = str(x)
+        
+        answerString = ""
+        
+        # Reverse each num by iterating through once
+        for i in range(0, len(numString)):
+            answerString = numString[i] + answerString
+        
+        answer = int(answerString)
+        
+        # check if negative
+        if negative is True:
+            answer = answer * -1
+        
+        if answer > (2**31 - 1):
+            return 0
+        elif answer < -2**31:
+            return 0
+            
+        return answer # O(n)
+ 
