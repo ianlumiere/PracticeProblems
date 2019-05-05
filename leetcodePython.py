1004117e11226d7703791f0558a065d99e83f5a8
Ian Lumiere <ianmlumiere@gmail.com>
2019-05-05 14:33:51 -0700

    Update leetcodePython.py

--- leetcodePython.py
+++ leetcodePython.py
@@ -759,3 +759,29 @@ def solution(A):
             return smallest
     
     return smallest
+
+#The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
+# +----+-------+--------+-----------+
+# | Id | Name  | Salary | ManagerId |
+# +----+-------+--------+-----------+
+# | 1  | Joe   | 70000  | 3         |
+# | 2  | Henry | 80000  | 4         |
+# | 3  | Sam   | 60000  | NULL      |
+# | 4  | Max   | 90000  | NULL      |
+# +----+-------+--------+-----------+
+# Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
+# +----------+
+# | Employee |
+# +----------+
+# | Joe      |
+# +----------+
+
+SELECT
+    e.name AS "Employee"  # I added the name to satisfy one of the requirements, but it is not necessary
+FROM employee e
+INNER JOIN employee m on e.managerId = m.id  # join the table on itself for all ees that have managers
+WHERE e.managerId IS NOT NULL
+    and e.salary > m.salary
+
