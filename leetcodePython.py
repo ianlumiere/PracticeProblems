0d8d07a0cca0927aa02305e9b1eaf8a8e6205b50
Ian Lumiere <ianmlumiere@gmail.com>
2019-05-05 10:50:26 -0700

    Update leetcodePython.py

--- leetcodePython.py
+++ leetcodePython.py
@@ -727,3 +727,35 @@ class Solution:
                 i = i + 1
         
         return len(nums)
+
+
+#Write a function:
+
+#def solution(A)
+
+#that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+#For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+#Given A = [1, 2, 3], the function should return 4.
+
+#Given A = [−1, −3], the function should return 1.
+
+#Write an efficient algorithm for the following assumptions:
+
+#N is an integer within the range [1..100,000];
+#each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+def solution(A):
+    smallest = 1
+    A.sort()
+    
+    for i in A:
+        if i <= 0:
+            pass
+        elif i <= smallest:
+            smallest = i + 1
+        else:
+            return smallest
+    
+    return smallest
