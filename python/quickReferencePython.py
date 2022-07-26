68636e3046462870892112cae8cbba09bebc65f1
Ian Huang-Lumiere <lumie@Ians-MacBook-Air.local>
2022-07-26 17:21:10 -0500

    fixing spacing

--- python/quickReferencePython.py
+++ python/quickReferencePython.py
@@ -18,32 +18,32 @@ print("\n\n   LOOPS")
 print("\n range for loop")
 # print each number from 0 to 2 (3 excluded)
 for i in range(0,3):  # this won't print 3
-  print(i)
+    print(i)
 
 print("\n use indexes of a list")
 pokemon = ["pikachu", "mew", "poliwhirl"]
 for i in range(len(pokemon)):
-  print(i)
+    print(i)
 
 print("\n iterate over list items")
 arr = ["apple", "banana", "cherry"]
 for i in arr:
-  print(i)
+    print(i)
 
 print("\n get both index and item during iteration")
 for index, item in enumerate(arr):
-  print(index, item)
+    print(index, item)
 
 print("\n iterate over key value pairs in a dictionary")
 dict_example = {"a": 1, "b": True, 300: [1,2,"zoo"]}
 for key, value in dict_example.items():
-  print(key, value)
+    print(key, value)
 
 print("\n while loop")
 j = 3
-while(j != 0):  # this won't print 0
-  print(j)
-  j = j - 1
+while j != 0:  # this won't print 0
+    print(j)
+    j = j - 1
 
 print("\n\n   SORTING")
 print("\n sort a list reverse alphabetically")
@@ -137,15 +137,15 @@ print(list(dict_1.values())) # gets all values in the dictionary and puts it in 
 print(list(dict_1.keys())) # gets all keys and puts it in a list
 del dict_1["key"] # removes the key value pair at the specified key
 for key, value in dict_1.items():
-  print(key, value)
+    print(key, value)
 
 print("\n\n   TRY/EXCEPT/FINALLY")
 try:
-  x = 7/0
+    x = 7/0
 except Exception as e:
-  print(e)
+    print(e)
 finally:
-  print("finally happens no matter what")
+    print("finally happens no matter what")
 
 print("\n\n   MAP")
 print("\n maps will execute a function on all elements of a list")
