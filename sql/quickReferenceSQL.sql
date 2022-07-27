3c4611fd0cf6c7acbedf4d69703347a66d751fe5
Ian Huang-Lumiere <lumie@Ians-MacBook-Air.local>
2022-07-27 13:54:35 -0500

    sql

--- sql/quickReferenceSQL.sql
+++ sql/quickReferenceSQL.sql
@@ -5,3 +5,9 @@ FROM department d
 LEFT JOIN employee e on d.id = e.dept_id
 GROUP BY d.name
 ORDER BY num_ee DESC, d.name -- this will order by the number of ee's from high to low, then by name alphabetically if num is tied
+
+SELECT
+    name
+FROM Products
+LIMIT 3 OFFSET 5; -- returns 3 rows starting from row 5
+
